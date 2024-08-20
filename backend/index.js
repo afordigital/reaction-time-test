6e1b3792a2fc8b6f1e4933de645bfc16b3274bde
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2024-08-21 00:22:06 +0200

    feat: modify db

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { createClient } from "@supabase/supabase-js";
-import cors from 'cors'
-
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
@@ -20,6 +19,17 @@ app.get("/", async (req, res) => {
 });
 
 // update user
+app.patch("/update/:id", async (req, res) => {
+    
+  await supabase.from("users").update({ id: req.params.id, score: req.body.score }).eq('name', req.body.name);
+  res.status(200).send({ res: "ok" });
+});
+
+// insert user
+app.post("/insert", async (req, res) => {
+  await supabase.from("users").insert({ name: req.body.user, score: req.body.score });
+  res.status(200).send({ res: "ok" });
+});
 
 app.listen(3000, () => {
   console.log("server listening...");
