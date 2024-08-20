40690acbbee2f6dfe0deeed217a7af2facee2375
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2024-08-20 22:50:28 +0200

    feat: new server in express

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send({res: "ok"})
+});
+
+app.listen(3000, () => {
+  console.log("server listening...");
+});
