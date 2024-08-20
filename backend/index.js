172fa59e1f7409e1760800fc5859a8f129a3632f
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2024-08-20 23:24:33 +0200

    feat: add supabase in backend LETSGO

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,25 @@
 import express from "express";
+import { createClient } from "@supabase/supabase-js";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).send({res: "ok"})
+const supabase = createClient(process.env.PROJECT_URL, process.env.API_KEY);
+
+// get users
+app.get("/", async (req, res) => {
+  const users = await supabase
+    .from("users")
+    .select()
+    .order("score", { ascending: true })
+    .limit(10);
+  res.status(200).send({ data: users.data });
 });
+
+// update user
 
 app.listen(3000, () => {
   console.log("server listening...");
 });
 
-export default app
+export default app;
